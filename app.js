41c617f448eff184d9dd8cab38a9f44c7f359a33
Koodies <kwtan@outlook.sg>
2022-06-06 11:56:22 +0800

    init passport

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require("express");
 const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
+const passport = require('./src/lib/passport')
 const mongoConnect = require('./src/lib/mongoConnect')
 
 const usersRouter = require("./routes/users");
@@ -14,6 +15,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
+app.use(passport.initialize);
 
 app.use("/users", usersRouter);
 
